3b557288a228f181c07cd94d61608c8af90b78ff
Luca Pegolotti <lpego@stanford.edu>
2021-11-30 17:37:52 -0800

    Add create_venv.sh

---
+++ create_venv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# create venv:
+VENVNAME=gromenv
+virtualenv --python=python3 $VENVNAME
+
+source $VENVNAME/bin/activate
+
+# requirements:
+pip install matplotlib
+pip install vtk
+pip install scipy
+pip install dgl
+pip install torch
